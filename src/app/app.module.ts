2ddc494348eb040af61dab6e972773865222495c
Granddeveolop <dragonofdev@yahoo.com>
2017-09-14 11:52:42 -0700

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainboardComponent } from './mainboard/mainboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    DashboardComponent,
+    MainboardComponent
   ],
   imports: [
     BrowserModule
